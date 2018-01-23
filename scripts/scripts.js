0ec7a209dc392c4090e6822e668396ce6c57c82a
Victor <vitya.lebed.84@mail.ru>
2018-01-24 02:30:28 +0300

    sbdU

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -30,19 +30,55 @@ $('#nav a[href^="#"]').click(function(event) {
     event.preventDefault();
 
 });
+
+
+function getTargetLeft(elem) {
+
+    //gets the id of the section header
+    //from the navigation's href e.g. ("#html")
+    var id = elem.attr('href');
+
+    //Gets the distance from the top and 
+    //subtracts the height of the nav.
+    console.log($(id))
+    if ($(id).offset()) {
+        return $(id).innerWidth();
+    }
+    return 0
+
+}
+
+//Smooth scroll when user click link that starts with #
+$('.scroll a[href^="#"]').click(function(event) {
+
+    //gets the distance from the top of the 
+    //section refenced in the href.
+    var target = getTargetLeft($(this));
+    //scrolls to that section.
+    console.log(target)
+
+    $('#services').animate({
+        scrollLeft: target
+    }, 700);
+
+    //prevent the browser from jumping down to section.
+    event.preventDefault();
+
+});
+
 $(document).ready(function() {
 
     $(".cross").hide();
-    $(".menu").hide();
+    $(".sub-menu").hide();
     $(".hamburger").click(function() {
-        $(".menu").slideToggle("slow", function() {
+        $(".sub-menu").slideToggle("slow", function() {
             $(".hamburger").hide();
             $(".cross").show();
         });
     });
 
     $(".cross").click(function() {
-        $(".menu").slideToggle("slow", function() {
+        $(".sub-menu").slideToggle("slow", function() {
             $(".cross").hide();
             $(".hamburger").show();
         });
